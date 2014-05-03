17905be6c6ba4c6ddd7c8ea4200066b31c3ace54
James Conroy-Finn <james@logi.cl>
2014-05-03 20:09:17 +0100

    Add projectile

---
+++ lisp/init-projectile.el
@@ -0,0 +1,9 @@
+;;; init-projectile --- Install and configure projectile
+;;; Commentary:
+;;; Code:
+
+(require-package 'projectile)
+(projectile-global-mode)
+
+p(provide 'init-projectile)
+;;; init-projectile.el ends here
