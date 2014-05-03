8d8177bfd90087b128413b6d82a20a3b508dcc0f
James Conroy-Finn <james@logi.cl>
2014-05-03 19:28:18 +0100

    Use Monokai theme

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -1,18 +1,11 @@
 (when (< emacs-major-version 24)
   (require-package 'color-theme))
 
-(require-package 'color-theme-sanityinc-solarized)
-(require-package 'color-theme-sanityinc-tomorrow)
+(require-package 'monokai-theme)
 
 ;;------------------------------------------------------------------------------
 ;; Old-style color theming support (via color-theme.el)
 ;;------------------------------------------------------------------------------
-(defcustom window-system-color-theme 'color-theme-sanityinc-solarized-dark
-  "Color theme to use in window-system frames.
-If Emacs' native theme support is available, this setting is
-ignored: use `custom-enabled-themes' instead."
-  :type 'symbol)
-
 (defcustom tty-color-theme 'color-theme-terminal
   "Color theme to use in TTY frames.
 If Emacs' native theme support is available, this setting is
@@ -22,7 +15,7 @@ ignored: use `custom-enabled-themes' instead."
 (unless (boundp 'custom-enabled-themes)
   (defun color-theme-terminal ()
     (interactive)
-    (color-theme-sanityinc-solarized-dark))
+    (color-theme-monokai))
 
   (defun apply-best-color-theme-for-frame-type (frame)
     (with-selected-frame frame
@@ -45,31 +38,16 @@ ignored: use `custom-enabled-themes' instead."
 ;;------------------------------------------------------------------------------
 
 ;; If you don't customize it, this is the theme you get.
-(setq-default custom-enabled-themes '(sanityinc-solarized-light))
+(setq-default custom-enabled-themes '(monokai))
 
 ;; Ensure that themes will be applied even if they have not been customized
 (defun reapply-themes ()
   "Forcibly load the themes listed in `custom-enabled-themes'."
   (dolist (theme custom-enabled-themes)
     (unless (custom-theme-p theme)
-      (load-theme theme)))
+      (load-theme theme t)))
   (custom-set-variables `(custom-enabled-themes (quote ,custom-enabled-themes))))
 
 (add-hook 'after-init-hook 'reapply-themes)
-
-
-;;------------------------------------------------------------------------------
-;; Toggle between light and dark
-;;------------------------------------------------------------------------------
-(defun light ()
-  "Activate a light color theme."
-  (interactive)
-  (color-theme-sanityinc-solarized-light))
-
-(defun dark ()
-  "Activate a dark color theme."
-  (interactive)
-  (color-theme-sanityinc-solarized-dark))
-
 
 (provide 'init-themes)
