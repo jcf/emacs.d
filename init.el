7f5dab4c4c5b4cd49401dfa05811aeab735eed80
James Conroy-Finn <james@logi.cl>
2014-05-09 11:18:48 +0100

    Add slim-mode

--- init.el
+++ init.el
@@ -101,14 +101,16 @@
 
 ;; Extra packages which don't require any configuration
 
-(require-package 'gnuplot)
-(require-package 'lua-mode)
-(require-package 'feature-mode)
-(require-package 'htmlize)
 (require-package 'dsvn)
+(require-package 'feature-mode)
+(require-package 'gnuplot)
+(require-package 'htmlize)
+(require-package 'lua-mode)
+(require-package 'regex-tool)
+(require-package 'slim-mode)
+
 (when *is-a-mac*
   (require-package 'osx-location))
-(require-package 'regex-tool)
 
 ;;----------------------------------------------------------------------------
 ;; Allow access from emacsclient
