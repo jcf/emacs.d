73f3c6c3cf15e72c269f96c22ac7cdf23fde8129
James Conroy-Finn <james@logi.cl>
2014-05-20 16:21:52 +0100

    Disable global-magic-wip-save-mode
    It doesn't work well when editing a file in a non-existent directory.

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -26,9 +26,9 @@
 
 ;;; When we start working on git-backed files, use git-wip if available
 
-(after-load 'magit
-  (global-magit-wip-save-mode)
-  (diminish 'magit-wip-save-mode))
+;; (after-load 'magit
+;;   (global-magit-wip-save-mode)
+;;   (diminish 'magit-wip-save-mode))
 
 (after-load 'magit
   (diminish 'magit-auto-revert-mode))
