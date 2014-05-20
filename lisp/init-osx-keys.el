03c692544453c9bab08872e212dfe75063f55db8
James Conroy-Finn <james@logi.cl>
2014-05-20 16:30:57 +0100

    Add OS X style copy/paste bindings

--- lisp/init-osx-keys.el
+++ lisp/init-osx-keys.el
@@ -12,6 +12,8 @@
   (after-load 'nxml-mode
     (define-key nxml-mode-map (kbd "M-h") nil))
   (global-set-key (kbd "M-ˍ") 'ns-do-hide-others) ;; what describe-key reports for cmd-option-h
+  (global-set-key (kbd "M-v") 'cua-paste)
+  (global-set-key (kbd "M-c") 'cua-copy-region)
   )
 
 
