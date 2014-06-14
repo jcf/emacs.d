408de83420c6f826016c8a6815e33b703e2dd59a
James Conroy-Finn <james@logi.cl>
2014-06-14 17:28:38 +0100

    Add smart-mode-line config
    It's not enabled…

---
+++ lisp/init-smart-mode-line.el
@@ -0,0 +1,10 @@
+;;; init-smart-mode-line.el --- Sets up an improved mode line
+;;; Commentary:
+;;; Code:
+(require-package 'smart-mode-line)
+
+(setq sml/theme nil)
+(sml/setup)
+
+(provide 'init-smart-mode-line)
+;;; init-smart-mode-line.el ends here
