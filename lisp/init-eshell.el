6417acaebfea88af88a281596019eb6909fc5702
James Conroy-Finn <james@logi.cl>
2014-07-02 13:31:31 +0100

    Tidy up init-eshell
    - Remove custom prompt code that does nothing
    - Remove eshell/clear function

--- lisp/init-eshell.el
+++ lisp/init-eshell.el
@@ -9,17 +9,5 @@
 (setq eshell-error-if-no-glob t)
 (setq eshell-glob-case-insensitive t)
 
-(defun eshell/clear ()
-  "Clears all history within the current eshell buffer."
-  (let ((inhibit-read-only t))
-    (erase-buffer)))
-
-;; Custom eshell prompt
-(defun jcf/eshell-prompt ()
-  (concat (propertize (abbreviate-file-name (eshell/pwd)) 'face 'eshell-prompt)
-          " "))
-
-(setq eshell-prompt-function 'jcf/eshell-prompt)
-
 (provide 'init-eshell)
 ;;; init-eshell.el ends here
