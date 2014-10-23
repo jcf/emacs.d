6db46442131c3848c9a26eb95fc1caaf90f1b62c
James Conroy-Finn <james@logi.cl>
2014-10-23 21:56:26 +0100

    Use a hyphen to separate namespace from function
    From http://www.emacswiki.org/emacs/Namespaces:
    
    > You should choose a short word to distinguish your program from other
    > Lisp programs. The names of all global variables, constants, and
    > functions in your program should begin with that chosen
    > prefix. Separate the prefix from the rest of the name with a hyphen,
    > `-`.
    >
    > This practice helps avoid name conflicts, since all global variables
    > in Emacs Lisp share the same name space, and all functions share
    > another name space.
    >
    > Use two hyphens to separate prefix and name if the symbol is not meant
    > to be used by other packages.
    
    More coding conventions can be found in the [Emacs manual][1]
    
    [1]: http://www.gnu.org/software/emacs/manual/html_node/elisp/Coding-Conventions.html#Coding-Conventions

--- init.el
+++ init.el
@@ -1,24 +1,24 @@
 
 ;; [[file:~/.emacs.d/README.org::*Benchmarks][Benchmarks:1]]
 
-(defun jcf/time-subtract-millis (b a)
+(defun jcf-time-subtract-millis (b a)
   (* 1000.0 (float-time (time-subtract b a))))
 
-(defvar jcf/require-times nil
+(defvar jcf-require-times nil
   "A list of (FEATURE . LOAD-DURATION). LOAD-DURATION is the time
   taken in milliseconds to load FEATURE.")
 
 (defadvice require
   (around build-require-times (feature &optional filename noerror) activate)
-  "Note in `jcf/require-times' the time taken to require each feature."
+  "Note in `jcf-require-times' the time taken to require each feature."
   (let* ((already-loaded (memq feature features))
          (require-start-time (and (not already-loaded) (current-time))))
     (prog1
         ad-do-it
       (when (and (not already-loaded) (memq feature features))
-        (add-to-list 'jcf/require-times
+        (add-to-list 'jcf-require-times
                      (cons feature
-                           (jcf/time-subtract-millis (current-time)
+                           (jcf-time-subtract-millis (current-time)
                                                            require-start-time))
                      t)))))
 
@@ -49,31 +49,31 @@
 (require 'org-install)
 (require 'ob-tangle)
 
-(defvar jcf/config-dir
+(defvar jcf-config-dir
   (file-name-directory (or load-file-name (buffer-file-name))))
 
-(defun jcf/load-org (s)
+(defun jcf-load-org (s)
   (org-babel-load-file
-   (expand-file-name (format "init-%s.org" s) jcf/config-dir)))
+   (expand-file-name (format "init-%s.org" s) jcf-config-dir)))
 
 (add-hook
  'after-init-hook
  (lambda ()
-   (jcf/load-org "defuns")
-   (jcf/load-org "ubiquitous")
-   (jcf/load-org "genesis")
-   (jcf/load-org "presentation")
-   (jcf/load-org "evil")
-   (jcf/load-org "helm")
+   (jcf-load-org "defuns")
+   (jcf-load-org "ubiquitous")
+   (jcf-load-org "genesis")
+   (jcf-load-org "presentation")
+   (jcf-load-org "evil")
+   (jcf-load-org "helm")
    (when *is-a-mac*
-     (jcf/load-org "osx"))
-   (jcf/load-org "org")
-   (jcf/load-org "packages")
-   (jcf/load-org "window-management")
-   (jcf/load-org "version-control")
-   (jcf/load-org "languages")
-   (jcf/load-org "locales")
+     (jcf-load-org "osx"))
+   (jcf-load-org "org")
+   (jcf-load-org "packages")
+   (jcf-load-org "window-management")
+   (jcf-load-org "version-control")
+   (jcf-load-org "languages")
+   (jcf-load-org "locales")
 
-   (jcf/log-startup-time)))
+   (jcf-log-startup-time)))
 
 ;; Load\ Org\ files:1 ends here
